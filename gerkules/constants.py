d401806a514a27085adc1674aaec23b9e8698a2c
Mykyta-Harashchenko <160050977+Mykyta-Harashchenko@users.noreply.github.com>
2024-10-02 11:11:46 +0100

    Delete gerkules directory

--- gerkules/constants.py
+++
@@ -1,8 +0,0 @@
-from config.config import config
-
-TOKEN=config.get("BOT_TOKEN")
-
-
-WEBHOOK_PATH=f"/bot/{TOKEN}"
-
-WEBHOOK_URL=f"{config.get('NGROK_URL')}{WEBHOOK_PATH}"
--- gerkules/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class FormHotline(StatesGroup):
-    title = State()
-
-class FormWeather(StatesGroup):
-    title = State()
